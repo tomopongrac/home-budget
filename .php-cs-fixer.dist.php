c72a43703ce471c7784b8fc63135adbfc8d5b026
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2023-11-06 22:06:29 +0100

    Configuring friendsofphp/php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,17 @@
 <?php
 
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
+    ->in([__DIR__.'/src', __DIR__.'/tests'])
     ->exclude('var')
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'phpdoc_annotation_without_dot' => false,
+        'phpdoc_summary' => false,
+        'yoda_style' => true,
     ])
     ->setFinder($finder)
 ;
