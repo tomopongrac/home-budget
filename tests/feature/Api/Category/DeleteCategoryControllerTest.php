22e300e25a826e2b92a1aeae88bfc75374183be6
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2023-11-11 00:39:42 +0100

    Adding more test to delete category endpoint

--- tests/feature/Api/Category/DeleteCategoryControllerTest.php
+++ tests/feature/Api/Category/DeleteCategoryControllerTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Tests\feature\Api\Category;
 
+use App\Entity\Category;
 use App\Factory\CategoryFactory;
+use App\Factory\UserFactory;
 use App\Tests\ApiTestCase;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
@@ -22,5 +25,49 @@ class DeleteCategoryControllerTest extends ApiTestCase
         $this->baseKernelBrowser()
             ->delete('/api/categories/'.$category->getId())
             ->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    /** @test */
+    public function userCanDeleteHisCategory(): void
+    {
+        $user = UserFactory::createOne()->object();
+        $category = CategoryFactory::createOne([
+            'name' => 'Category name',
+            'user' => $user,
+        ])->object();
+
+        $this->authenticateUserInBrowser($user)
+            ->delete('/api/categories/'.$category->getId())
+            ->assertStatus(Response::HTTP_NO_CONTENT);
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = static::getContainer()->get('doctrine.orm.entity_manager') ?? throw new \RuntimeException('The doctrine.orm.entity_manager service should exist.');
+        $categoryRepository = $entityManager->getRepository(Category::class);
+        $categoryInDB = $categoryRepository->find($category->getId());
+        // check that category is deleted from database
+        $this->assertNull($categoryInDB);
+    }
+
+    /** @test */
+    public function userCantDeleteCategoryFromOtherUser(): void
+    {
+        $user = UserFactory::createOne()->object();
+        $otherUser = UserFactory::createOne()->object();
+        $category = CategoryFactory::createOne([
+            'name' => 'Category name',
+            'user' => $otherUser,
+        ])->object();
+
+        $this->authenticateUserInBrowser($user)
+            ->delete('/api/categories/'.$category->getId())
+            ->assertStatus(Response::HTTP_FORBIDDEN);
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = static::getContainer()->get('doctrine.orm.entity_manager') ?? throw new \RuntimeException('The doctrine.orm.entity_manager service should exist.');
+        $categoryRepository = $entityManager->getRepository(Category::class);
+        $categoryInDB = $categoryRepository->find($category->getId());
+        // check that category is not deleted from database
+        $this->assertNotNull($categoryInDB);
+        $this->assertEquals($category->getId(), $categoryInDB->getId());
     }
 }
