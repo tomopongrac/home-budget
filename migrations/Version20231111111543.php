8d52f502ddb20e32e71cdf4958820ab47e00aaad
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2023-11-11 14:27:05 +0100

    Creating migration for transaction entity

---
+++ migrations/Version20231111111543.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231111111543 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE transaction (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, title VARCHAR(255) NOT NULL, amount_cents INT NOT NULL, active_at DATETIME NOT NULL, type ENUM("income", "expense") NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATE DEFAULT NULL, INDEX IDX_723705D112469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D112469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE transaction DROP FOREIGN KEY FK_723705D112469DE2');
+        $this->addSql('DROP TABLE transaction');
+    }
+}
